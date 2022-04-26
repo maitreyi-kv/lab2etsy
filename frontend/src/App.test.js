83b0a9210d9b10759964812e97fd079a7fcf7b08
Maitreyi Kunnavakkam Vinjimur <98665151+maitreyi-kv@users.noreply.github.com>
2022-04-25 22:12:59 -0700

    add react test

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,9 +1,33 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import Product from './components/Product/Product';
+import React from 'react';
+
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/Etsy Part 2/i);
   expect(linkElement).toBeInTheDocument();
+
+
+});
+
+describe('Landing Page', () => {
+  const initialState = {  };
+  const mockStore = configureStore();
+  let store;
+
+  it('Shows Register Login when user hasnt logged in yet', () => {
+    store = mockStore(initialState);
+    const { getByText } = render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+
+    expect(getByText('Etsy')).not.toBeNull();
+    expect(getByText('Register')).not.toBeNull();
+    expect(getByText('Login')).not.toBeNull();
+  });
 });
